e778efaca1e7f00c8faecf6c56528b782b9ff37f
Iraproza <kolomiira@gmail.com>
2021-02-16 19:11:26 +0200

    Start with product models

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path")
 const shopRoutes = require("./routes/shopRoutes");
-// const errorController = require("./")
 
 const PORT = 8000;
 const app = express(); 
@@ -12,5 +11,4 @@ app.set("views", "views");
 app.use(express.static(path.join(__dirname, "static")));
 
 app.use(shopRoutes);
-// app.use(errorController.get404)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
